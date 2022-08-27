9e80a676315095e8ac31562ce08114c297b0bb28
d-doshyn <d.doshyn@gmail.com>
2022-08-27 21:40:38 +0300

    пофиксили проблему с бургером

--- wp-content/themes/expand/assets/js/burger.js
+++ wp-content/themes/expand/assets/js/burger.js
@@ -2,9 +2,12 @@ let navMenuTrigger = document.querySelector(".burger");
 let burgerMenu = document.querySelector(".burger-menu");
 
 navMenuTrigger.onclick = () => {
-    if (burgerMenu.style.display = "none") {
-        burgerMenu.style.display = "flex";   
+    if (burgerMenu.classList.contains('flex')) {
+        burgerMenu.classList.remove('flex');   
+        burgerMenu.classList.add('none');   
+        console.log(burgerMenu);
     } else {
-        burgerMenu.style.display = "none";
+        burgerMenu.classList.remove("none");
+        burgerMenu.classList.add("flex");
     }
 }
