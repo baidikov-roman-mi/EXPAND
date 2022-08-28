ff836cfa86af438f7927c3af27c859837e448f4c
d-doshyn <d.doshyn@gmail.com>
2022-08-28 14:06:23 +0300

    доделал хедер

--- wp-content/themes/expand/assets/js/burger.js
+++ wp-content/themes/expand/assets/js/burger.js
@@ -4,8 +4,7 @@ let burgerMenu = document.querySelector(".burger-menu");
 navMenuTrigger.onclick = () => {
     if (burgerMenu.classList.contains('flex')) {
         burgerMenu.classList.remove('flex');   
-        burgerMenu.classList.add('none');   
-        console.log(burgerMenu);
+        burgerMenu.classList.add('none');
     } else {
         burgerMenu.classList.remove("none");
         burgerMenu.classList.add("flex");
